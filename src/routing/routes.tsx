de03c126fd32a2c87bff9e18866196c1645f852d
Nick <nickphilomath@gmail.com>
2024-01-16 00:22:26 +0500

    added logout functionality

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,15 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
+import Home from "../components/Home";
 import Login from "../components/Login";
+import Logout from "../components/Logout";
 import Drivers from "../components/Drivers";
 
 const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
-  { path: "/logout", element: <h1>Logout</h1> },
+  { path: "/logout", element: <Logout /> },
   {
     path: "/",
     element: <Layout />,
     children: [
+      { path: "", element: <Home /> },
       { path: "drivers", element: <Drivers /> },
       { path: "trucks", element: <h1>trucks</h1> },
     ],
